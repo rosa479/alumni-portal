0c58734e4b7302e0427e84330248eefae57939dd
shub-krishan208 <shubhamkrishan999@gmail.com>
2025-10-03 08:18:33 +0530

    api endpoints initialized

---
+++ test_app/urls.py
@@ -0,0 +1,14 @@
+# test_app/urls.py
+
+from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# test_app/views.py
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    """
+    API view for user registration.
+    """
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny] # Anyone can access this view to register
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # We can return some basic user info upon successful registration
+        user_data = UserSerializer(user).data
+        
+        return Response(
+            {
+                "user": user_data,
+                "message": "User created successfully. Please log in.",
+            },
+            status=status.HTTP_201_CREATED
+        )
