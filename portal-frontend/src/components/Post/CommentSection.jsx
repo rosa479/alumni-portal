452e905a2bc89db2c5e8d385622778e4a28a200d
shub-krishan208 <shubhamkrishan999@gmail.com>
2025-10-07 20:06:53 +0530

    fixed import issue

--- portal-frontend/src/components/Post/CommentSection.jsx
+++ portal-frontend/src/components/Post/CommentSection.jsx
@@ -1,15 +1,21 @@
 // src/features/posts/components/CommentSection.jsx
-import React from 'react';
-import Comment from '../post/Comment';
+import React from "react";
+import Comment from "./Comment";
 
 function CommentSection({ comments }) {
   return (
     <div className="bg-white p-6 rounded-xl shadow-lg mt-8">
-      <h3 className="text-xl font-bold text-dark-text mb-6">{comments.length} Comments</h3>
-      
+      <h3 className="text-xl font-bold text-dark-text mb-6">
+        {comments.length} Comments
+      </h3>
+
       {/* Add a Comment Form */}
       <div className="flex items-start gap-4 mb-8">
-        <img src="https://i.pravatar.cc/150?u=a042581f4e29026704d" alt="My Profile" className="w-10 h-10 rounded-full" />
+        <img
+          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+          alt="My Profile"
+          className="w-10 h-10 rounded-full"
+        />
         <div className="flex-1">
           <textarea
             className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-blue focus:outline-none transition"
@@ -24,7 +30,7 @@ function CommentSection({ comments }) {
 
       {/* List of Comments */}
       <div className="space-y-6">
-        {comments.map(comment => (
+        {comments.map((comment) => (
           <Comment
             key={comment.id}
             authorName={comment.authorName}
