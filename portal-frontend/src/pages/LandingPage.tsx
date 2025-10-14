35bbcdb804575e29403de35e4e06dc23bf76a7eb
shub-krishan208 <shubhamkrishan999@gmail.com>
2025-10-14 23:24:31 +0530

    font added

--- portal-frontend/src/pages/LandingPage.tsx
+++ portal-frontend/src/pages/LandingPage.tsx
@@ -266,7 +266,7 @@ const HeroSection = () => {
       <div className="relative w-full  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-0.5">
           <div className="space-y-8">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-white">
+            <h1 className="font-heading text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-white">
               Welcome to the IIT Kharagpur Community
             </h1>
             <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold leading-tight text-white">
