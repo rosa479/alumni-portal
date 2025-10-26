2f7a12301a510d0784537ca8e84cabeb09f04b27
shub-krishan208 <shubhamkrishan999@gmail.com>
2025-10-26 23:54:13 +0530

    changed admin endpoint

--- alum_net/urls.py
+++ alum_net/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django/', admin.site.urls),
     path('api/', include('test_app.urls')),
     path('api/scholarships/', include('scholarships.urls')),
 ]
